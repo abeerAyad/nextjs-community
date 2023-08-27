555d2873917e04604c63c06c92b2cf47883623e7
HamdyPs <redwanhmdyrdwan@gmail.com>
2023-08-27 18:38:47 +0300

    change save element schema in commentId to array of id and add map in save element route to get all comment id Relates #10

--- src/app/api/savedElement/[id]/route.js
+++ src/app/api/savedElement/[id]/route.js
@@ -1,6 +1,5 @@
-import Post from "@/models/social/post";
 import Comment from "@/models/social/post/comment";
-import savedElements from "@/models/social/post/savedElement";
+import SavedElements from "@/models/social/post/savedElement";
 import User from "@/models/user";
 import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
@@ -8,19 +7,19 @@ import { NextResponse } from "next/server";
 export async function POST(req, { params: { id } }) {
     const { user } = await getServerSession(req)
     const userData = await User.findOne({ email: user.email })
-    const commentId = await Comment.find({postId: id})
+    const commentId = await Comment.find({ postId: id }, { _id: 1 })
+    const data = commentId.map((comm) => comm._id)
 
     try {
-        const savedElement = await savedElements.create({
-            commentId,
+        const savedElement = await SavedElements.create({
             postId: id,
-            userId: userData._id
+            userId: userData._id,
+            commentId: data,
         })
         return NextResponse.json({ msg: 'Saved Element successfully', savedElement }, { status: 201 })
 
     } catch (error) {
-        console.log(error)
         return NextResponse.json({ error: error.message }, { status: 500 })
-}
+    }
 }
 
--- src/models/social/post/savedElement.js
+++ src/models/social/post/savedElement.js
@@ -1,24 +1,24 @@
-import { model, models, Schema, mongoose } from "mongoose";
+import { model, models, Schema } from "mongoose";
 
 const savedElementsSchema = new Schema({
-   postId: {
-    type: Schema.Types.ObjectId,
-        ref:'posts'
-    },
-    userId:{
+    postId: {
         type: Schema.Types.ObjectId,
-        ref:'users'
+        ref: 'posts'
     },
-    commentId: {
+    userId: {
         type: Schema.Types.ObjectId,
-        ref:'comments'
+        ref: 'users'
     },
+    commentId: [{
+        type: Schema.Types.ObjectId,
+        ref: 'comments'
+    }],
     createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+        type: Date,
+        default: Date.now,
+    },
 })
 
-const savedElements = models.savedElements || model('savedElements', savedElementsSchema)
+const SavedElements = models.savedElements || model('savedElements', savedElementsSchema)
 
-export default savedElements;
+export default SavedElements;
